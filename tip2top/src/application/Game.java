faf8a981d7f6c8fc001997aa13a6b75e6cbd8480
Dylan <dylan.leclair@icloud.com>
2019-03-01 09:55:54 -0700

    Organized images, updated graphics for menu.

--- tip2top/src/application/Game.java
+++ tip2top/src/application/Game.java
@@ -2,6 +2,8 @@ package application;
 	
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+
+import javafx.animation.Animation.Status;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.event.EventHandler;
@@ -22,20 +24,20 @@ import javafx.scene.text.Text;
 
 public class Game extends Application {
 
-	int clickCount = 9;
+	int clickCount = 9; // starts at 9, since stackpane loads from front to back. 
 	Stage window;
 	Scene menusc, opening, loadsc, helpsc;
 	
 	
 	/**
-	 * Loads opening scene onto a StackPane
+	 * Loads opening scene onto a StackPane, reading files named "screen(digit)" from path.
 	 * @param pane - the stackpane to load images onto, from back to front.
 	 */
 	public static void loadOpening (StackPane pane) {
 		for (int i = 10; i >= 1; i--) {
 			Image image;
 			try {
-				image = new Image(new FileInputStream("./src/application/introimg/screen" + i + ".jpg"));
+				image = new Image(new FileInputStream("./src/application/resources/introimg/screen" + i + ".jpg"));
 			    ImageView imageView = new ImageView(image); 
 			    pane.getChildren().add(imageView);
 			} catch (FileNotFoundException e) {
@@ -48,8 +50,10 @@ public class Game extends Application {
 	 * @param node a node, to be translated off the screen.
 	 */
 	public static void fadeImageDown (Node node) {
+		
+		
 		TranslateTransition translate = new TranslateTransition();
-		translate.setDuration(Duration.millis(3000)); 
+		translate.setDuration(Duration.millis(2500)); 
 		translate.setNode(node);
 		translate.setByY(720);
 		translate.setCycleCount(1); 
@@ -74,14 +78,21 @@ public class Game extends Application {
 			BorderPane menu = new BorderPane();
 			menusc = new Scene(menu,1280,720);
 			
-			Text title = new Text("Tip to the Top");
+			Text title = new Text("");
 			title.setFont(new Font(120));
 			
 			VBox menuButtons = new VBox(20);
-			Button newGame = new Button("New Game");
-			Button loadGame = new Button("Load Game");
-			Button help = new Button("Help");
+			Button newGame = new Button();
+			Button loadGame = new Button();
+			Button help = new Button();
 			
+			
+			Image newimg = new Image(new FileInputStream("./src/application/resources/menuimg/newgame.jpg"));
+			newGame.setGraphic(new ImageView(newimg));
+			Image loadimg = new Image(new FileInputStream("./src/application/resources/menuimg/loadgame.jpg"));
+			loadGame.setGraphic(new ImageView(loadimg));
+			Image helpimg = new Image(new FileInputStream("./src/application/resources/menuimg/help.jpg"));
+			help.setGraphic(new ImageView(helpimg));
 			
 			// Event handling for menu buttons
 			
@@ -91,9 +102,9 @@ public class Game extends Application {
 			
 			// Styling menu buttons (replace with images later)
 			
-			newGame.setStyle("-fx-font: 30 arial;");
-			loadGame.setStyle("-fx-font: 30 arial;");
-			help.setStyle("-fx-font: 30 arial;");
+			newGame.setStyle("-fx-base: #000000;");
+			loadGame.setStyle("-fx-base: #000000;");
+			help.setStyle("-fx-base: #000000;");
 			
 			menuButtons.getChildren().addAll(newGame, loadGame, help);
 
@@ -104,7 +115,7 @@ public class Game extends Application {
 			BorderPane.setMargin(menuButtons, new Insets(40,0,0,150));
 			BorderPane.setMargin(title, new Insets(70, 0, 0, 120));
 			
-			String image = Game.class.getResource("west.jpg").toExternalForm();
+			String image = Game.class.getResource("menu.jpg").toExternalForm();
 			menu.setStyle("-fx-background-image: url('" + image + "'); " +
 			           "-fx-background-position: center center; " +
 			           "-fx-background-repeat: stretch;");
@@ -113,10 +124,14 @@ public class Game extends Application {
 			StackPane openingPane = new StackPane(); // Container objects for this scene
 			opening = new Scene(openingPane,1280,720); // Creates actual scene
 		    
+			// Make it impossible to start next animation unless current finished 
+			
 			loadOpening(openingPane);
+			
 			opening.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
 			    @Override
 			    public void handle(MouseEvent mouseEvent) {
+
 			    	/*  calls fadeImageDown on an object in openingPane when the mouse is pressed.
 			    	 *  (remember that these are the images added by loadOpening() )
 			    	 *  This is performed once per image, for a total of 10 times. */
@@ -144,8 +159,11 @@ public class Game extends Application {
 			
 			menusc.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
+			// Loading and starting the screen. 
+			
 			window.setScene(menusc);
 			window.setTitle("Tip to the Top");
+			window.setResizable(false);
 			window.show();
 			
 			
@@ -153,7 +171,10 @@ public class Game extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+	/**
+	 * Main function, executes the program. A boolean switch can change between versions (we've opted for full GUI) 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		boolean textV = false;
 		
